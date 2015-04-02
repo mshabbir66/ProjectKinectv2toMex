9b11f74bc03693bb4ddeacb716258c1563d60767
Shabbir Marzban <smarzban13@ku.edu.tr>
2015-04-03 00:02:09 +0300

    s
    s

--- ProjectMex/K4Wv2OpenCVModule.h
+++ ProjectMex/K4Wv2OpenCVModule.h
@@ -16,7 +16,84 @@
 using namespace std;
 using namespace cv;
 
+class  CK4Wv2OpenCVModule
+{
+	// Sensor frame data values
+	// Color frame resolution
 
+	bool provideColor;
+	bool provideBody;
+	bool provideFace;
+
+	INT64                   m_nStartTime;
+	INT64                   timeStamp;
+
+public:
+	static const int COLOR_FRAME_WIDTH = 1920;
+	static const int COLOR_FRAME_HEIGHT = 1080;
+	// Depth frame resolution
+	static const int DEPTH_FRAME_WIDTH = 512;
+	static const int DEPTH_FRAME_HEIGHT = 424;
+	// Infrared frame resolution
+	static const int INFRARED_FRAME_WIDTH = 512;
+	static const int INFRARED_FRAME_HEIGHT = 424;
+
+	CK4Wv2OpenCVModule(bool provideColor, bool provideBody, bool provideFace);
+	~CK4Wv2OpenCVModule();
+
+	HRESULT InitializeKinectDevice();
+
+	// Image frame Mat
+	Mat colorRAWFrameMat;
+	Mat facialAnimationParameters;
+	Mat bodyParameters;
+	bool faceFlag = FALSE;
+	bool bodyFlag = FALSE;
+
+	// Process frame
+	void UpdateData();
+	INT64 GetTimeStamp();
+	void ExtractFaceRotationInDegrees(const Vector4* pQuaternion, float* pPitch, float* pYaw, float* pRoll);
+	Point2f CK4Wv2OpenCVModule::BodyToScreen(const CameraSpacePoint& bodyPoint);
+	float* bodyBuffer;
+	// Calculate Mapped Frame
+	ICoordinateMapper*      m_pCoordinateMapper;
+
+	int* facialAnimationParametersBuffer;
+
+	//ushort* pDepthRAWBuffer;
+	//ushort* pInfraRAWBuffer;
+private:
+	// Device
+	IKinectSensor* pSensor;
+	RGBQUAD* pColorRAWBuffer;
+
+
+
+
+	// Frame reader
+	IMultiSourceFrameReader* pMultiSourceFrameReader;
+	//Face Readers
+
+	// Face sources
+	IFaceFrameSource*		m_pFaceFrameSources[BODY_COUNT];
+
+	// Face readers
+	IFaceFrameReader*		m_pFaceFrameReaders[BODY_COUNT];
+
+	// HD Face Sources
+	IHighDefinitionFaceFrameSource* m_pHDFaceFrameSources[BODY_COUNT];
+
+	// HDFace readers
+	IHighDefinitionFaceFrameReader*		m_pHDFaceFrameReaders[BODY_COUNT];
+
+	// Release function
+	template< class T > void SafeRelease(T** ppT);
+	void ProcessFaces(IBody** ppBodies);
+	void CK4Wv2OpenCVModule::ProcessBody(IBody** ppBodies);
+};
+
+/*
 class CK4Wv2OpenCVModule
 {
 
@@ -71,7 +148,7 @@ private:
 
 	// Release function
 	template< class T > void SafeRelease(T** ppT);
-
+*/
 };
 
 #endif
